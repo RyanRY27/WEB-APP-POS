2aa3f6b42a23a6d7f6cdbcae611d669381f32f3d
RyanRY27 <129965623+RyanRY27@users.noreply.github.com>
2023-05-03 21:48:34 +0800

    Update admin.php

--- pos/app/code/admin.php
+++ pos/app/code/admin.php
@@ -9,6 +9,12 @@ if ($tab == "products")
 	$productClass = new Product();
 	$products = $productClass->query("select * from products order by id desc");
 
+}	
+if(Auth::access('owner')){
+	require viewpath('admin/admin');
 }
+else{
 
-require viewpath('admin/admin');
+	Auth::set_message("You don't have access to ADMIN PAGE!!");
+	require viewpath('auth/security');
+}
