76651aa23fdb4fb50d637f496dd57035a894ef5a
RyanRY27 <129965623+RyanRY27@users.noreply.github.com>
2023-05-17 20:02:42 +0800

    Delete app directory

--- app/views/partials/foot.view.php
+++
@@ -1,8 +0,0 @@
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
-</body>
-<script></script>
-<script src="assets/js/all.js"></script>
-</html>
