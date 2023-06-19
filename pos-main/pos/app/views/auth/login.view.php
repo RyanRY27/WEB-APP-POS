c4eb84c2ce89a4f988b54113fea49fbbe3bd7cfb
TechnoDeserter <87742707+TechnoDeserter@users.noreply.github.com>
2023-06-19 22:14:39 +0800

    Add files via upload

--- pos-main/pos/app/views/auth/login.view.php
+++ pos-main/pos/app/views/auth/login.view.php
@@ -1,43 +1,27 @@
-<?php require viewpath('partials/head');?>
+<?php require viewpath('partials/head'); ?>
 
-<div class="container-fluid border col-lg-4 col-md-5 mt-5 mb-3 p-4 shadow">
-
-        <form method="post">
-            <center>
-                <h1><i class="fa fa-user-alt"></i></h1>
-                <h3>Log In</h3>
-                <div><?=esc(APP_NAME)?></div>
-            </center>
-            <br>
-
-            <div class="form-floating mb-3">
-                <input name="email" type="email" class="form-control <?=!empty($errors['email']) ? 'border-danger':''?>" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-                <?php if(!empty($errors['email'])):?>
-                    <small class="text-danger"><?=$errors['email']?></small>
-                <?php endif;?>
+<body>
+    <div class="container" >
+        <form class="login-form">
+            <div class="logo-container"></div>
+            <h2>Login</h2>
+            <div class="form-group">
+                <input type="email" id="username" name="username" required>
+                <label for="username">Email</label>
             </div>
-            <div class="form-floating">
-                <input name="password" type="password" class="form-control <?=!empty($errors['password']) ? 'border-danger':''?>" id="floatingPassword" placeholder="password">
-                <label for="floatingPassword">Password</label>
-                <?php if(!empty($errors['password'])):?>
-                    <small class="text-danger"><?=$errors['password']?></small>
-                <?php endif;?>
-            </div> 
-            <div class="col-auto">
-                <div class="mt-2 form-check">
-                <input class="form-check-input" type="checkbox" id="autoSizingCheck2">
-                <label class="form-check-label" for="autoSizingCheck2">
-                    Remember me
-                </label>
-                </div>
-            </div>  
-                    
-            <br>
-            <div class="row">
-                <button class="btn btn-primary" style="font-size: 16px">Log In</button>
+            <div class="form-group">
+                <input type="password" id="password" name="password" required>
+                <label for="password">Password</label>
             </div>
-        </form> 
-</div>
-    
-<?php require viewpath('partials/foot');?>
+            <div class="form-group">
+                <input type="checkbox" id="remember-me" name="remember-me">
+                <label for="remember-me">Remember Me</label>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+</body>
+
+<?php require viewpath('partials/foot'); ?>
