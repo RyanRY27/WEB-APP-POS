b3f513182f200572c7776f3329eff0bc98822e43
TechnoDeserter <87742707+TechnoDeserter@users.noreply.github.com>
2023-06-23 18:52:57 +0800

    Add files via upload

--- pos-main/pos/app/views/auth/login.view.php
+++ pos-main/pos/app/views/auth/login.view.php
@@ -1,17 +1,23 @@
 <?php require viewpath('partials/head'); ?>
 
-<body>
-    <div class="container" >
-        <form class="login-form">
+<div class="frame1">
+    <div class="container">
+        <form class="login-form" method="POST">
             <div class="logo-container"></div>
             <h2>Login</h2>
             <div class="form-group">
-                <input type="email" id="username" name="username" required>
-                <label for="username">Email</label>
+                <input  class="form-control <?=!empty($errors['email']) ? 'border-danger':''?>" type="email" id="email" name="email" required>
+                <label for="email">Email</label>
+                <?php if(!empty($errors['email'])):?>
+                    <small class="text-danger"><?=$errors['email']?></small>
+                <?php endif;?>
             </div>
             <div class="form-group">
                 <input type="password" id="password" name="password" required>
                 <label for="password">Password</label>
+                <?php if(!empty($errors['password'])):?>
+                    <small class="text-danger"><?=$errors['password']?></small>
+                <?php endif;?>
             </div>
             <div class="form-group">
                 <input type="checkbox" id="remember-me" name="remember-me">
@@ -22,6 +28,8 @@
             </div>
         </form>
     </div>
-</body>
+</div>
+    
+
 
 <?php require viewpath('partials/foot'); ?>
